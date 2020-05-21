d1e303721be167524e82f58f730ba8912ade3624
empty-works <pama.mark@gmail.com>
2020-05-21 03:21:20 +0200

    IsPalindrome function now normalizes the char characters being pushed into the stack and queue.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,20 @@
 #include <queue>
 #include <cctype>
 #include <vector>
+#include <string>
 
-bool IsPalindrome() {
+bool IsPalindrome(const std::string &palin) {
+	
+	bool is_palindrome {true};
+	std::stack<char> pal_stack {};
+	std::queue<char> pal_queue {};
+	for(auto &pal_char : palin) {
+		if(std::isalpha(pal_char)) {
+			pal_stack.push(std::toupper(pal_char));
+			pal_queue.push(std::toupper(pal_char));
+		}
+		//if(pal_stack.pop() == 
+	}
 	
 }
 
