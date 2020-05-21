bb35c833cdbd312b072af5b43125f2e9a57e3dfc
empty-works <pama.mark@gmail.com>
2020-05-21 05:06:25 +0200

    main function now displays if the string is a palindrome. Fixed issue with the IsPalindrome function.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include <cctype>
 #include <vector>
 #include <string>
+#include <iomanip>
 
 bool IsPalindrome(const std::string &palin) {
 	
-	bool is_palindrome {true};
 	std::stack<char> pal_stack {};
 	std::queue<char> pal_queue {};
 	for(auto &pal_char : palin) {
@@ -15,22 +15,34 @@ bool IsPalindrome(const std::string &palin) {
 			pal_stack.push(std::toupper(pal_char));
 			pal_queue.push(std::toupper(pal_char));
 		}
-		if(!pal_stack.empty() && !pal_queue.empty()) {
-			if(pal_stack.top() != pal_queue.front()) {
-				is_palindrome = false;
-				break;
-			}
-			else {
-				pal_stack.pop();
-				pal_queue.pop();
-			}
+	}
+	while(!pal_stack.empty() && !pal_queue.empty()) {
+		if(pal_stack.top() != pal_queue.front()) {
+			return false;
+		}
+		else {
+			pal_stack.pop();
+			pal_queue.pop();
 		}
 	}
-	return is_palindrome;	
+	
+	return true;	
 }
 
 int main() {
-
 	
+	std::vector<std::string> pal_vec {
+		"a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana", "avid diva", "Amore, Roma", "A Toyota's a toyota", 
+		"A Santa at NASA", "C++", "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", 
+		"palindrome"
+	};
+
+	std::cout << std::boolalpha;
+	std::cout << std::setw(8) << std::left << "Result" << "String" << std::endl;
+	for(const auto &s : pal_vec) {
+		std::cout << std::setw(8) << std::left << IsPalindrome(s) << s << std::endl;
+	}
+	
+	std::cout << std::endl;
 	return 0;
 }
